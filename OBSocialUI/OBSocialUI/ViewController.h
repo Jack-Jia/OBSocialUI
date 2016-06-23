e36028ff577504256baeca8953f962f6c740d2e8
DarrenW <sdauwangzh@163.com>
2016-06-23 14:01:45 +0800

    Create Project

---
+++ OBSocialUI/OBSocialUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBSocialUI
+//
+//  Created by DarrenW on 16/6/23.
+//  Copyright © 2016年 oneBug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OBSocialUI/OBSocialUI/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OBSocialUI
+//
+//  Created by DarrenW on 16/6/23.
+//  Copyright © 2016年 oneBug. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
