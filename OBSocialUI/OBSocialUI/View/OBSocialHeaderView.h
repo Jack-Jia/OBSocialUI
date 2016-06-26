1d3bf994e18846bc5d3d90dca62ed540e48fe44f
DarrenW <sdauwangzh@163.com>
2016-06-26 17:57:27 +0800

    添加主界面的顶部视图

---
+++ OBSocialUI/OBSocialUI/View/OBSocialHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  OBSocialHeaderView.h
+//  OBSocialUI
+//
+//  Created by DarrenW on 16/6/26.
+//  Copyright © 2016年 oneBug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBSocialHeaderView : UIView
+
+@end
